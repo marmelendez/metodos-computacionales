46c77b67a1e43532c617b922990a07b40896c02f
Maribel Melendez <lizbethmelendez@live.com>
2021-03-02 14:34:02 -0600

    Solution of problem 6

--- Actividad2.2_Parte2.scm
+++ Actividad2.2_Parte2.scm
@@ -3,4 +3,18 @@
 
 //Problema 3
 
+
 //Problema 6
+(define (deep-reverse lista)
+    (if (not (list? lista)) 
+        'not_list
+        (recursive-reverse lista)
+    )
+)
+
+(define (recursive-reverse lista)
+    (cond
+        ((not (list? lista)) lista)
+        (else (reverse (map recursive-reverse lista)))
+    )
+)
