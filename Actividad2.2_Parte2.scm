e211c0d12c90d6e8aabf775c4e374c1e30f99e9c
Maribel Melendez <lizbethmelendez@live.com>
2021-03-04 17:55:27 -0600

    Correction to problem 6: using append

--- Actividad2.2_Parte2.scm
+++ Actividad2.2_Parte2.scm
@@ -5,16 +5,12 @@
 
 
 //Problema 6
-(define (deep-reverse lista)
-    (if (not (list? lista)) 
-        'not_list
-        (recursive-reverse lista)
-    )
-)
-
-(define (recursive-reverse lista)
-    (cond
-        ((not (list? lista)) lista)
-        (else (reverse (map recursive-reverse lista)))
+(define (deep-reverse l)
+    (if (null? l)
+        '()
+        (if (list? (car l))
+            (append (deep-reverse (cdr l)) (list (deep-reverse (car l))))
+            (append (deep-reverse (cdr l)) (list (car l)))
+        )
     )
 )
