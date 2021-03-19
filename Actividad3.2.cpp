bf6597f5b28c0a96938d3bd5f92a0d0b1f0d8d72
Maribel Melendez <lizbethmelendez@live.com>
2021-03-18 19:08:27 -0600

    Save every line of the .txt file

--- Actividad3.2.cpp
+++ Actividad3.2.cpp
@@ -1,30 +1,32 @@
-#include <iostream>
-#include <fstream>
+#include<iostream>
+#include<fstream>
+#include<string>
 #include<vector>
 
 using namespace std;
 
-vector<char> leerArchivo(){
-    vector<char> expresiones;
-    char c;
+vector<string> leerArchivo(){
+    vector<string> expresiones;
+    string linea;
     string nombreArchivo = "expresiones.txt";
     ifstream archivo(nombreArchivo.c_str());
 
     if(archivo.fail()){
-        cout<<"Error, no pudimos abrir el archivo de texto"<<endl;
+        cout<<"Error, no pudimos abrir el archivo de texto :("<<endl;
         exit(1);
     }
 
-    while (archivo.get(c))  {
-        expresiones.push_back(c);
-    } 
+    while (!archivo.eof()){
+        getline(archivo,linea);
+        expresiones.push_back(linea);
+    }
 
     archivo.close();
-    
+
     return expresiones;
 }
 
-void imprimirArchivo(vector<char> expresiones){
+void imprimirArchivo(vector<string> expresiones){
     for (int i=0; i<expresiones.size(); i++){
         cout<<expresiones[i]<<endl;
     }
@@ -35,7 +37,7 @@ void lexerAritmetico(string archivo){
 }
 
 int main(){
-    vector<char> expresiones = leerArchivo();
+    vector<string> expresiones = leerArchivo();
     imprimirArchivo(expresiones);
     return 0; 
 }
