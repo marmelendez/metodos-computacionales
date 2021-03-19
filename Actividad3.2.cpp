fe6223201a7429a71d277b9257e8a736fe2dfd6c
Maribel Melendez <lizbethmelendez@live.com>
2021-03-18 18:45:31 -0600

    Read a .txt file and save each character in a vector

--- Actividad3.2.cpp
+++ Actividad3.2.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
+#include <fstream>
+#include<vector>
 
 using namespace std;
 
-int main(){
+vector<char> leerArchivo(){
+    vector<char> expresiones;
+    char c;
+    string nombreArchivo = "expresiones.txt";
+    ifstream archivo(nombreArchivo.c_str());
+
+    if(archivo.fail()){
+        cout<<"Error, no pudimos abrir el archivo de texto"<<endl;
+        exit(1);
+    }
+
+    while (archivo.get(c))  {
+        expresiones.push_back(c);
+    } 
+
+    archivo.close();
     
+    return expresiones;
+}
+
+void imprimirArchivo(vector<char> expresiones){
+    for (int i=0; i<expresiones.size(); i++){
+        cout<<expresiones[i]<<endl;
+    }
+}
+
+void lexerAritmetico(string archivo){
+
+}
+
+int main(){
+    vector<char> expresiones = leerArchivo();
+    imprimirArchivo(expresiones);
+    return 0; 
 }
