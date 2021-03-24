15bb015c25a429eafab17d21dd94ba43b3efe060
Maribel Melendez <lizbethmelendez@live.com>
2021-03-23 19:41:02 -0600

    Add enum Tipo file

--- AnalizadorLexico/Tipo.java
+++ AnalizadorLexico/Tipo.java
@@ -1,5 +1,18 @@
 package AnalizadorLexico;
 
 public enum Tipo {
-    
+    VARIABLE,
+    ASIGNACION,
+    SUMA,
+    RESTA,
+    MULTIPLICACION,
+    DIVISION,
+    COMENTARIO,
+    POTENCIA,
+    PARENTESIS_QUE_ABRE,
+    PARENTESIS_QUE_CIERRA,
+    ENTERO,
+    REAL,
+    REALEXP,
+    ERROR   
 }
