d127cd282c05e792a707322acc89b664a35247c7
Maribel Melendez <lizbethmelendez@live.com>
2021-03-23 19:43:19 -0600

    Add attributes, constructor, setters and getters methods to the Token class

--- AnalizadorLexico/Token.java
+++ AnalizadorLexico/Token.java
@@ -2,4 +2,31 @@ package AnalizadorLexico;
 
 public class Token {
     
+    //ATRIBUTOS
+    Tipo tipo;
+    String valor;
+
+    //METODOS
+
+    /* Constructor */
+    Token (){
+        this.tipo = Tipo.ERROR; 
+        this.valor = " ";
+    }
+    /* Getters y setters */
+    public String getValor() {
+        return this.valor;
+    }
+
+    public Tipo getTipo() {
+        return this.tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
 }
