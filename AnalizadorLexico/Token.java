13929976be95a30b36aa5bc12a7b0db4750b763e
Maribel Melendez <lizbethmelendez@live.com>
2021-03-23 19:49:42 -0600

    Function indiceAlfabeto

--- AnalizadorLexico/Token.java
+++ AnalizadorLexico/Token.java
@@ -29,4 +29,37 @@ public class Token {
     public void setValor(String valor) {
         this.valor = valor;
     }
+
+    /* Obtiene índice en la tabla de transición que representa un símbolo */
+    public int indiceAlfabeto(char simbolo){
+        int indice = -1;
+        if (simbolo == 'E' || simbolo == 'e'){
+            indice = 11;
+        } else if (Character.isLetter(simbolo)){
+            indice = 0;
+        } else if (Character.isDigit(simbolo)){
+            indice = 1;
+        } else if (simbolo == '_'){
+            indice = 2;
+        } else if (simbolo == '='){
+            indice = 3;
+        } else if (simbolo == '+'){
+            indice = 4;
+        } else if (simbolo == '-'){
+            indice = 5;
+        } else if (simbolo == '*'){
+            indice = 6;
+        } else if (simbolo == '/'){
+            indice = 7;
+        } else if (simbolo == '^'){
+            indice = 8;
+        } else if (simbolo == '('){
+            indice = 9;
+        } else if (simbolo == ')'){
+            indice = 10;
+        } else if (simbolo == '.'){
+            indice = 12;
+        }
+        return indice;
+    }
 }
